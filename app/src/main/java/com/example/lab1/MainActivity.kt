15e64d95da39754076cd5589fe0622083049979c
gurami152 <gurami152@gmail.com>
2020-08-26 23:53:11 +0300

    create renderer and surface view

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.lab1
 
+import android.opengl.GLSurfaceView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var gLView: GLSurfaceView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        gLView = MyGLSurfaceView(this)
+        setContentView(gLView)
+
     }
 }
