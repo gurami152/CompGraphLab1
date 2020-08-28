5cc324686554ff67026aa0089a6a1a8a892d9460
gurami152 <gurami152@gmail.com>
2020-08-28 15:56:13 +0300

    define Triangle.kt and Square.kt and draw them

--- app/src/main/java/com/example/lab1/MyGLRenderer.kt
+++ app/src/main/java/com/example/lab1/MyGLRenderer.kt
@@ -7,18 +7,30 @@ import javax.microedition.khronos.opengles.GL10
 
 class MyGLRenderer : GLSurfaceView.Renderer {
 
+    private lateinit var mTriangle: Triangle
+
+    private lateinit var mSquare: Square
+
+
+
     override fun onSurfaceCreated(unused: GL10?, p1: javax.microedition.khronos.egl.EGLConfig?) {
         // Set the background frame color
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
+        mTriangle = Triangle()
+        mSquare = Square()
     }
 
     override fun onDrawFrame(unused: GL10) {
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+        mTriangle.draw()
+        mSquare.draw()
     }
 
     override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
         GLES20.glViewport(0, 0, width, height)
     }
+
+
 
 }
